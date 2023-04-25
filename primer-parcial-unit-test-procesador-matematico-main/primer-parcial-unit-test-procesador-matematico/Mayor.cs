cfdb421974471291ca0e2dd4aa96bf691fa46e00
NoeDuarte <noeliaduarte.95@gmail.com>
2023-04-25 19:02:47 -0300

    Se agrego clase Mayor

---
+++ primer-parcial-unit-test-procesador-matematico-main/primer-parcial-unit-test-procesador-matematico/Mayor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_parcial_unit_test_procesador_matematico
+{
+    internal class Mayor
+    {
+    }
+}
